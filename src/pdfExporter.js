248f1d16e350fb5ac97f6c12c9b9430672728e56
Keeko <787018661@qq.com>
2023-07-31 19:03:04 +0800

    [feat] func getImage

--- src/pdfExporter.js
+++ src/pdfExporter.js
@@ -6,7 +6,95 @@ const html2CanvasDefaultOption = {
   taintTest: false,
   useCORS: true,
   scale: 2,
-  logging: false
+  logging: false,
+};
+
+async function getImageBase64Url(element) {
+  const { scrollWidth, scrollHeight } = element;
+  const canvasWidth = scrollWidth + 10;
+  const canvasHeight = scrollHeight + 10;
+  const options = {
+    scale: 2,
+    width: canvasWidth,
+    height: canvasHeight,
+    useCORS: true,
+    allowTaint: true,
+    taintTest: false,
+    logging: false,
+  };
+  const canvas = await html2Canvas(element, options);
+  return canvas.toDataURL("image/jpeg", 1.0);
+}
+
+async function getImage(title, { element, text, logo }) {
+  const imageUrl = await getImageBase64Url(element);
+
+  // 创建外层容器元素 <div class="image-export-template">
+  const container = document.createElement("div");
+  container.classList.add("image-export-template");
+  container.style.padding = "20px 20px 0";
+  container.style.minWidth = "800px";
+
+  // 创建图片元素 <img class="content" src="imageUrl" />
+  const image = document.createElement("img");
+  image.classList.add("content");
+  image.src = imageUrl; // 设置图片的 URL
+
+  // 创建分割线元素 <div class="cutline"></div>
+  const cutline = document.createElement("div");
+  cutline.classList.add("cutline");
+  cutline.style.width = "100%";
+  cutline.style.borderBottom = "1px solid #eee";
+
+  // 创建底部元素 <div class="footer">
+  const footer = document.createElement("div");
+  footer.classList.add("footer");
+  footer.style.height = "50px";
+  footer.style.display = "flex";
+  footer.style.justifyContent = "space-between";
+  footer.style.alignItems = "center";
+
+  // 创建左侧底部文本元素 <span class="footer-left">页面数据来源于wind</span>
+  const footerLeft = document.createElement("span");
+  footerLeft.classList.add("footer-left");
+  footerLeft.textContent = text;
+  footerLeft.style.fontSize = "12px";
+  footerLeft.style.color = "#999";
+
+  // 创建右侧底部图片元素 <img class="footer-right" :src="vue2logo" />
+  const footerRight = document.createElement("img");
+  footerRight.classList.add("footer-right");
+  footerRight.src = logo; // 设置图片的 URL
+  footerRight.style.width = "40px";
+  footerRight.style.height = "40px";
+
+  // 将元素添加到 DOM 树中
+  footer.appendChild(footerLeft);
+  footer.appendChild(footerRight);
+  container.appendChild(image);
+  container.appendChild(cutline);
+  container.appendChild(footer);
+
+  const { scrollWidth, scrollHeight } = container;
+  const canvasWidth = scrollWidth;
+  const canvasHeight = scrollHeight;
+
+  const opts = {
+    scale: 2,
+    width: canvasWidth,
+    height: canvasHeight,
+    useCORS: true,
+    allowTaint: true,
+    taintTest: false,
+    logging: false,
+  };
+  const canvas = await html2Canvas(element, opts);
+  const imgUrl = canvas.toDataURL("image/jpeg", 1.0);
+
+  const link = document.createElement("a");
+  link.href = imgUrl;
+  link.download = title;
+  link.click();
 }
 
 // ##getSinglePagePdf
@@ -29,7 +117,7 @@ function getSinglePagePdf(title, element, scrollYNum) {
           height:
             scrollHeight - i * maxCanvasHeight < maxCanvasHeight
               ? scrollHeight - i * maxCanvasHeight + bufferHeight
-              : maxCanvasHeight
+              : maxCanvasHeight,
         })
       );
     }
@@ -38,11 +126,11 @@ function getSinglePagePdf(title, element, scrollYNum) {
       html2Canvas(element, {
         ...html2CanvasDefaultOption,
         y: 0,
-        scrollY
+        scrollY,
       })
     );
   }
-  return Promise.all(promise).then(canvas => {
+  return Promise.all(promise).then((canvas) => {
     const { scrollWidth } = element;
     let pageSizeArr = [];
     if (scrollWidth <= scrollHeight) {
@@ -56,7 +144,14 @@ function getSinglePagePdf(title, element, scrollYNum) {
       const contentWidth = canvas[i].width / canvasScale;
       const contentHeight = canvas[i].height / canvasScale;
       const pageData = canvas[i].toDataURL("image/jpeg", 1.0);
-      pdf.addImage(pageData, "jpeg", 0, totalHeight, contentWidth, contentHeight);
+      pdf.addImage(
+        pageData,
+        "jpeg",
+        0,
+        totalHeight,
+        contentWidth,
+        contentHeight
+      );
       totalHeight += contentHeight;
     }
     return pdf.save(`${title}.pdf`);
@@ -68,7 +163,7 @@ function getSinglePagePdf(title, element, scrollYNum) {
 const A4Width = 592.28;
 const A4Height = 841.89;
 
-function getElementAttrSize(element , attr) {
+function getElementAttrSize(element, attr) {
   return parseInt(window.getComputedStyle(element)[attr], 10);
 }
 
@@ -89,7 +184,6 @@ function createPageHeader(rootWidth, pageNum) {
 
   return div;
 }
-
 
 class PreWorker {
   constructor(rootElement) {
@@ -136,10 +230,14 @@ class PreWorker {
 
   insertElementToContainer(element) {
     if (!element) return;
-    const elementHeight = this.calcElementInA4PageHeight(getElementAttrSize(element, "height"));
+    const elementHeight = this.calcElementInA4PageHeight(
+      getElementAttrSize(element, "height")
+    );
 
     if (this.remainderHeight < elementHeight) {
-      this.loadedPromise.push(html2Canvas(this.A4Container, html2CanvasDefaultOption));
+      this.loadedPromise.push(
+        html2Canvas(this.A4Container, html2CanvasDefaultOption)
+      );
       this.pageNum += 1;
       this.initA4Container();
     }
@@ -151,36 +249,48 @@ class PreWorker {
 
   finishInsertElementToContainer() {
     const { children } = this.A4Container || {};
-    if (Object.keys(children).length !== 0) { 
-      this.loadedPromise.push(html2Canvas(this.A4Container, html2CanvasDefaultOption));
+    if (Object.keys(children).length !== 0) {
+      this.loadedPromise.push(
+        html2Canvas(this.A4Container, html2CanvasDefaultOption)
+      );
       this.removeContainerToRootElement();
     }
     return this.loadedPromise;
   }
-
 }
 
 function getMultiPagePdf(title, rootElement, skeletonArr) {
   const exporter = new PreWorker(rootElement);
-  skeletonArr.forEach(domId => {
+  skeletonArr.forEach((domId) => {
     const componentElement = rootElement.querySelector(`#${domId}`);
     exporter.insertElementToContainer(componentElement);
-  })
-  
-  return Promise.all(exporter.finishInsertElementToContainer()).then((canvas) => {
-    const pdf = new JsPDF("p", "pt", "a4", true);
-    for (let i = 0; i < canvas.length; i++) {
-      const contentWidth = canvas[i].width;
-      const contentHeight = canvas[i].height;
-      const imgHeight = (A4Width / contentWidth) * contentHeight;
-      const pageData = canvas[i].toDataURL("image/jpeg", 1.0);
-      pdf.addImage(pageData, "jpeg", 0, 0, A4Width, imgHeight, undefined, "SLOW");
-      if (i < canvas.length - 1) {
-        pdf.addPage();
-      }
-    }
-    return pdf.save(`${title}.pdf`);
   });
+
+  return Promise.all(exporter.finishInsertElementToContainer()).then(
+    (canvas) => {
+      const pdf = new JsPDF("p", "pt", "a4", true);
+      for (let i = 0; i < canvas.length; i++) {
+        const contentWidth = canvas[i].width;
+        const contentHeight = canvas[i].height;
+        const imgHeight = (A4Width / contentWidth) * contentHeight;
+        const pageData = canvas[i].toDataURL("image/jpeg", 1.0);
+        pdf.addImage(
+          pageData,
+          "jpeg",
+          0,
+          0,
+          A4Width,
+          imgHeight,
+          undefined,
+          "SLOW"
+        );
+        if (i < canvas.length - 1) {
+          pdf.addPage();
+        }
+      }
+      return pdf.save(`${title}.pdf`);
+    }
+  );
 }
 
-export { getSinglePagePdf, getMultiPagePdf }
+export { getImage, getSinglePagePdf, getMultiPagePdf };
